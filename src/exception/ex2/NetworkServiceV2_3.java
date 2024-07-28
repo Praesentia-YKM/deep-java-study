c30d528ebacb8c82e118e671a30aa0b585b460e1
Praesentia-YKM <68469694+Praesentia-YKM@users.noreply.github.com>
2024-07-28 22:41:59 +0900

    add java exception 예외클래스 생성 후 예외와 정상로직의 분리

--- src/exception/ex2/NetworkServiceV2_3.java
+++ src/exception/ex2/NetworkServiceV2_3.java
@@ -1,5 +1,12 @@
 package exception.ex2;
 
+/**
+ * 하나의 try안에 정상흐름을 모두 담는다.
+ * 예외부분은 catch에서 해결
+ *
+ * 이제 외부와 연결을 했으니 외부자원에 대한 반납처리만 신경써주면된다.
+ * connect와 send에서 예외가 발생해버리면 disconnect가 호출이 안되기 때문.
+ */
 public class NetworkServiceV2_3 {
 
     public void sendMessage(String data) {
