82d397f59399930904c8ce74f00c5c8148ff04ea
Praesentia-YKM <68469694+Praesentia-YKM@users.noreply.github.com>
2024-07-28 22:49:09 +0900

    add java exception finally의 등장개요

--- src/exception/ex2/NetworkServiceV2_4.java
+++ src/exception/ex2/NetworkServiceV2_4.java
@@ -14,6 +14,10 @@ public class NetworkServiceV2_4 {
             System.out.println("[오류] 코드: " + e.getErrorCode() + ", 메시지: " + e.getMessage());
         }
 
+        // disconnect는 무조건 호출 -> 어짜피 에러도 정상흐름으로 만들었으니까
+        // 하지만 내가 잡아둔 에러 외적으로 발생해버리면?? disconnect가 절대 실행이 안됨..
+        // 이런 구조로는 아무리 꼼꼼하게 예외를 다 catch하더라도 완벽하게 disconnect에 대한
+        // 처리 보장을 할 수가 없다... 다른 방식이 필요하다..!! 이래서 finally가 등장!!
         client.disconnect();
     }
 }
