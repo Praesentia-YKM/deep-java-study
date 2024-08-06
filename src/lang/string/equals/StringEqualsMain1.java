ec2b8659bd9acd92561f415433b7427564d39210
Praesentia-YKM <68469694+Praesentia-YKM@users.noreply.github.com>
2024-08-07 03:06:56 +0900

    add java exception String - new를 통한 생성과 리터럴 변수에 바로 꽂는 것의 차이점 - 문자열 풀의 개념

--- src/lang/string/equals/StringEqualsMain1.java
+++ src/lang/string/equals/StringEqualsMain1.java
@@ -5,9 +5,13 @@ public class StringEqualsMain1 {
     public static void main(String[] args) {
         String str1 = new String("hello"); //x001
         String str2 = new String("hello"); //x002
-        System.out.println("new String() == 비교: " + (str1 == str2));
-        System.out.println("new String() equals 비교:" + (str1.equals(str2)));
+        System.out.println("new String() == 비교: " + (str1 == str2)); // 동일성
+        System.out.println("new String() equals 비교:" + (str1.equals(str2))); // 동등성
 
+        // 문자열 최적화를 위해 자바에서 문자열 풀이라는 것을 제공(메모리 최적화 및 문자 생성시간 줄여줌)
+        // new를 안쓰면 문자열 풀에 바로 String값을 넣어버린다.
+        // -> 풀 안서 hello라는 값을 찾고(해쉬알고리즘사용해서 빠름) 이걸 계속 참조
+        // 프로그래밍에서  Pool이란 말은 공용 자원을 모아둔 곳이라는 것을 의미한다. 재사용 개념, 힙영역 사용
         String str3 = "hello"; //x003
         String str4 = "hello"; //x004
         System.out.println("리터럴 == 비교: " + (str3 == str4));
--- src/lang/string/equals/StringEqualsMain2.java
+++ src/lang/string/equals/StringEqualsMain2.java
@@ -1,5 +1,8 @@
 package lang.string.equals;
 
+/**
+ * 문자열 비교는 항상 동일성이아닌 동등성 비교를 해줘야한다.
+ */
 public class StringEqualsMain2 {
 
     public static void main(String[] args) {
@@ -14,6 +17,7 @@ public class StringEqualsMain2 {
 
     private static boolean isSame(String x, String y) {
         //return x == y;
+        // --> x==y로하면 개발자마다 new String으로 할지 리터럴 변수로 바로 넣은 걸 쓸지 알 수가 없어 결고가 달라질 수 있음
         return x.equals(y);
     }
 }
