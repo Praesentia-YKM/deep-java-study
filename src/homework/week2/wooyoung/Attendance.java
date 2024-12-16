b56a20c5e2022fc8557cade58b7e694b7eacc09b
Praesentia-YKM <ykm8864@naver.com>
2024-12-17 00:31:31 +0900

    java map homework week2 modified

--- src/homework/week2/wooyoung/Attendance.java
+++ src/homework/week2/wooyoung/Attendance.java
@@ -3,14 +3,13 @@ package homework.week2.wooyoung;
 import java.util.Date;
 
 public class Attendance {
-    private String empId;
-    private Date absentDate;
-    private String absentYn;
+    protected String empId;
+    protected Date absentDate;
+    protected String absentYn;
 
    	public Attendance(String empId, Date absentDate, String absentYn) {
    		this.empId = empId;
    		this.absentDate = absentDate;
    		this.absentYn = absentYn;
    	}
-
 }
