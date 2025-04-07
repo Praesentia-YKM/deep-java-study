fa8a5e6c4f256f87bbe3223753ab24f5117a6b70
Praesentia-YKM <ykm8864@naver.com>
2025-04-07 17:58:43 +0900

    add example 다양한 함수형인터페이스 와 메서드 참조에 대한 깊은 이해

---
+++ src/lamdaStreamOptional/lambda/lambda5/mystream/Ex2_Student.java
@@ -0,0 +1,56 @@
+package lamdaStreamOptional.lambda.lambda5.mystream;
+
+import lamdaStreamOptional.lambda.lambda5.filter.GenericFilter;
+import lamdaStreamOptional.lambda.lambda5.map.GenericMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex2_Student {
+
+    public static void main(String[] args) {
+        // 점수가 80점 이상인 학생의 이름을 추출해라.
+        List<Student> students = List.of(
+                new Student("Apple", 100),
+                new Student("Banana", 80),
+                new Student("Berry", 50),
+                new Student("Tomato", 40)
+        );
+
+        List<String> directResult = direct(students);
+        System.out.println("directResult = " + directResult);
+
+        List<String> lambdaResult = lambda(students);
+        System.out.println("lambdaResult = " + lambdaResult);
+    }
+
+    private static List<String> direct(List<Student> students) {
+        List<String> highScoreNames = new ArrayList<>();
+        for (Student student : students) {
+            if (student.getScore() >= 80) {
+                String name = student.getName();
+                highScoreNames.add(name);
+            }
+        }
+        return highScoreNames;
+    }
+
+    // 1. filter: 점수가 80점 이상인 학생들만 필터
+    // 2. map: 필터링된 학생 객체에서 이름만 추출
+    private static List<String> lambda(List<Student> students) {
+        List<Student> filtered = GenericFilter.filter(students, s -> s.getScore() >= 80);
+        List<String> mapped = GenericMapper.map(filtered, s -> s.getName());
+
+        return mapped;
+//        List<String> result = GenericMapper.map(students, s -> {
+//            // 필터 조건을 map 함수 내에 포함
+//            if (s.getScore() >= 80) {
+//                return s.getName();
+//            }
+//            return null;
+//        });
+//
+//        result.removeIf(name -> name == null);
+//        return result;
+    }
+}
